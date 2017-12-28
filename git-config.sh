7a598ba8cbdd6abc7eb9cc24f18d252dd948ba2b
jhhwang <jhhwang4195@gmail.com>
2017-12-28 10:29:27 +0900

    update git-config.sh

---
+++ git-config.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+git config --global user.name "jhhwang"
+git config --global user.email jhhwang4195@gmail.com
+git config --global color.ui auto
+git config --global merge.tool vimdiff
+
+git config --global alias.lg1 "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
+
+git config --global alias.lg2 "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n'' %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all"
