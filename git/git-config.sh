d5a284e9d0b047f599f87d2f0fb31eb3fba694fd
jhhwang <jhhwang4195@gmail.com>
2018-01-04 15:04:06 +0900

    Add print for git config info

--- git/git-config.sh
+++ git/git-config.sh
@@ -1,4 +1,12 @@
-#!/bin/sh
+#!/bin/bash
+echo -e "\e[1;32;40m########################################################\033[0m"
+echo -e "\e[1;32;40m# before git config setting\033[0m"
+echo -e "\e[1;32;40m########################################################\033[0m"
+git config --list
+
+########################################################
+# git config setting
+########################################################
 git config --global user.name "jhhwang"
 git config --global user.email jhhwang4195@gmail.com
 git config --global color.ui auto
@@ -7,3 +15,8 @@ git config --global merge.tool vimdiff
 git config --global alias.lg1 "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
 
 git config --global alias.lg2 "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n'' %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all"
+
+echo -e "\e[1;32;40m########################################################\033[0m"
+echo -e "\e[1;32;40m# after git config setting\033[0m"
+echo -e "\e[1;32;40m########################################################\033[0m"
+git config --list
