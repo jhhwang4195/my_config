bf6529fbb9437bbae448cbefb0c46fc0ecda2b6a
jhhwang <jhhwang4195@gmail.com>
2018-01-04 10:07:05 +0900

    add git-config2.sh

---
+++ git/git-config2.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+git config --global user.name "jhhwang"
+git config --global user.email jhhwang4195@gmail.com
+
+git config --global alias.lg "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
+
+git config --global color.branch auto
+git config --global color.diff auto
+git config --global color.status auto
+
+git config --global color.branch.current "yellow reverse"
+git config --global color.branch.local "yellow"
+git config --global color.branch.remote "green"
+
+git config --global color.diff.meta "yellow bold"
+git config --global color.diff.frag "megenta bold"
+git config --global color.diff.old "red bold"
+git config --global color.diff.new "green bold"
+
+git config --global color.status.added "yellow"
+git config --global color.status.changed "green"
+git config --global color.status.untracked "cyan"
+
+git config --global merge.tool vimdiff
