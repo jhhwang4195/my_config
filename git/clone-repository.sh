930e3dbaad3b9274a079853cf2c20122bbffda60
jhhwang <jhhwang4195@gmail.com>
2018-01-10 14:51:10 +0900

    Add clone-repository.sh

---
+++ git/clone-repository.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ROOT_DIR="git"
+GIT_USER="jhhwang4195"
+repos=("TIL" "my_source" "my_config" "commitbot" "slackbot")
+
+mkdir $ROOT_DIR
+cd $DIR
+
+for repo in "${repos[@]}"; do
+    echo -e "\e[44m >>> Cloning into $repo\e[0m"
+    git clone https://github.com/$GIT_USER/$repo
+done
