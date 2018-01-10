a4a896af319722947e68aca81f0258a02db5b947
jhhwang <jhhwang4195@gmail.com>
2018-01-10 15:12:40 +0900

    Add repository directory to clone

--- git/clone-repository.sh
+++ git/clone-repository.sh
@@ -5,9 +5,11 @@ GIT_USER="jhhwang4195"
 repos=("TIL" "my_source" "my_config" "commitbot" "slackbot")
 
 mkdir $ROOT_DIR
-cd $DIR
+cd $ROOT_DIR
 
 for repo in "${repos[@]}"; do
     echo -e "\e[44m >>> Cloning into $repo\e[0m"
-    git clone https://github.com/$GIT_USER/$repo
+    git clone https://github.com/$GIT_USER/$repo $repo
 done
+
+tree -d
